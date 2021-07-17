ca080ab8d279fec3c2b07f9eca034402f89d987d
rodom_mac <enjoying1018@naver.com>
2021-07-17 15:02:26 +0900

    component_scan, bean

---
+++ hello-spring/src/main/java/first/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package first.hellospring.service;
+
+import first.hellospring.domain.Member;
+import first.hellospring.repository.MemberRepository;
+import first.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        //중복 회원 검사
+        validDuplicateMember(member);
+        //회원 추가
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
