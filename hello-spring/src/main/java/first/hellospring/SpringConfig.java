a1470b218d73273790c3d2856a04f5f51500340c
rodom_mac <enjoying1018@naver.com>
2021-07-18 16:22:43 +0900

    jdbc

--- hello-spring/src/main/java/first/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/first/hellospring/SpringConfig.java
@@ -1,22 +1,31 @@
 package first.hellospring;
 
 import first.hellospring.repository.JdbcTemplateMemberRepository;
+import first.hellospring.repository.JpaMemberRepository;
 import first.hellospring.repository.MemberRepository;
 import first.hellospring.repository.MemoryMemberRepository;
 import first.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private EntityManager em;
 
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+/*
     private final DataSource dataSource;
 
     public SpringConfig(DataSource dataSource) {
         this.dataSource=dataSource;
     }
+
+ */
 
     @Bean
     public MemberService memberService(){
@@ -26,7 +35,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
