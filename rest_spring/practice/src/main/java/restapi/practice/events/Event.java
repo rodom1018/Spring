8b037819c0ea679dd5e5a0289044385200ef1c06
rodom_mac <enjoying1018@naver.com>
2021-08-10 00:10:12 +0900

    rest-lecture -chap2

--- rest_spring/practice/src/main/java/restapi/practice/events/Event.java
+++ rest_spring/practice/src/main/java/restapi/practice/events/Event.java
@@ -1,10 +1,26 @@
 package restapi.practice.events;
 
-import lombok.Builder;
+import lombok.*;
 
-@Builder
+import java.time.LocalDateTime;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of="id")
 public class Event {
 
     private String name;
     private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+
+    private Integer id;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStauts= EventStatus.DRAFT;
 }
--- rest_spring/practice/src/test/java/restapi/practice/events/EventTest.java
+++ rest_spring/practice/src/test/java/restapi/practice/events/EventTest.java
@@ -3,13 +3,25 @@ package restapi.practice.events;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class EventTest {
 
     @Test
     public void builder(){
         Event event = Event.builder().build();
-        Assertions.assertThat(event).isNotNull();
+        assertThat(event).isNotNull();
+    }
 
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Event";
+        event.setName(name);
+        event.setDescription("spring");
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo("spring");
     }
 
 }
