ef63aaa64eb59bdafec61a32701cdd547b27e7cc
rodom_mac <enjoying1018@naver.com>
2021-08-11 14:36:09 +0900

    body_bad request

--- rest_spring/practice/src/test/java/restapi/practice/events/EventControllerTests.java
+++ rest_spring/practice/src/test/java/restapi/practice/events/EventControllerTests.java
@@ -128,6 +128,7 @@ public class EventControllerTests {
         this.mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .content(this.objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].field").exists());
     }
 }
