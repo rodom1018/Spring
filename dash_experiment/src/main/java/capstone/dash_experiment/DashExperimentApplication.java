7c641d224995495fc847b1bab700caba4c025ffa
rodom <enjoying1018@naver.com>
2021-09-18 16:45:04 +0900

    experience of dash

---
+++ dash_experiment/src/main/java/capstone/dash_experiment/DashExperimentApplication.java
@@ -0,0 +1,13 @@
+package capstone.dash_experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashExperimentApplication.class, args);
+	}
+
+}
---
+++ dash_experiment/src/test/java/capstone/dash_experiment/DashExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.dash_experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
